66da9dc3725992322355765d4e02a9a77d57dabc
Osnago-L <osna.brera@gmail.com>
2022-03-07 13:54:36 +0100

    created table and column names(model-1stmigration)

---
+++ database/migrations/2022_03_07_124443_create_packages_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePackagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('packages', function (Blueprint $table) {
+            $table->id();
+            $table->string('package_name',100);
+            $table->string('location',50);
+            $table->string('hotel_name',50);
+            $table->string('air_company',50);
+            $table->string('flight_code',10);
+            $table->dateTimeTz('arrival', 0);
+            $table->dateTimeTz('departure', 0);
+            $table->integer('price');
+            $table->integer('discount');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('packages');
+    }
+}
